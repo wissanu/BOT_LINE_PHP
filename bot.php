f093e59ff8d7b0a3090d8ba2a336af967cb0de7b
wissanu <thungrut@gmail.com>
2017-06-27 08:55:28 +0700

    awake_v1

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "นอนเเล้วค้าบบบ ผม"
+				'text' => "ตื่นหยู่วววว"
 			];}
 
 			// Make a POST Request to Messaging API to reply to sender
